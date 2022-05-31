1801386b09473c722aaa3a5bb8ed9e174629d781
barbara-tshimologong <barbara@tshimologong.joburg>
2022-05-31 11:07:31 +0200

    Add project files.

---
+++ DataAccess/SqlServerAccess.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace DataAccess
+{
+    public class SqlServerAccess
+    {
+        public SqlServerAccess(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+         public string ConnectionString { get; init; }
+
+        public string ConnectToDb(string firstName,string surname, string printerCode)
+        {
+
+            string msg = String.Empty;
+
+            using (SqlConnection cn = new())
+            {
+                cn.ConnectionString = ConnectionString;
+                cn.Open();
+                using SqlCommand cmd = new();
+                cmd.Connection = cn;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "spInsert_Facilitator";
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@Surname", surname);
+                cmd.Parameters.AddWithValue("@PrinterCode", printerCode);
+                cmd.Parameters.AddWithValue("@Msg", msg);
+
+
+                cmd.ExecuteNonQuery();
+            }
+                return "Success";
+
+
+        }
+    }
+}
---
+++ IKAFCentral.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace IKAFCentral.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cs = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = AFCentral; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            DataAccess.SqlServerAccess ssa = new DataAccess.SqlServerAccess(cs);
+
+            
+            System.Console.WriteLine("Please enter your First Name");
+
+            var firstName = System.Console.ReadLine();
+
+            System.Console.WriteLine("Please enter your Surname");
+
+            var surname = System.Console.ReadLine();
+
+            System.Console.WriteLine("Please enter your PrinterCode");
+
+            var printerCode = System.Console.ReadLine();
+
+            System.Console.WriteLine(ssa.ConnectToDb(firstName, surname, printerCode));
+
+        }
+    }
+}
